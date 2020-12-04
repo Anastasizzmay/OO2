3a3d4de124a65f9a7f7fbdda29facbc9ee5bc837
Anastasiya <71728264+Anastasizzmay@users.noreply.github.com>
2020-12-04 14:08:01 +0300

    Add files via upload

---
+++ ООП Лр 1.cpp
@@ -0,0 +1,18 @@
+﻿#include<Windows.h>
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	cout << "Введите символ (строчную или прописную букву алфавита)" << endl;
+	char lit;
+	cin >> lit;
+	int k;
+	k = (int)lit;
+	cout << k << " " << lit << endl; 
+	cout << lit <<  " " << k << " " << hex << k << " " << oct << endl;
+}
